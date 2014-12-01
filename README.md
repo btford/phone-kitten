40d4054bbb6cb3a55753fce567bae107c31bc001
Brian Ford <btford@umich.edu>
2014-12-01 14:38:43 -0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# phone-kitten
+
+Angular phonecat tutorial re-written with the WIP [New Angular Router](angular/router).
+
+
+## Running the App
+
+```shell
+npm install
+npm start
+```
+
+Then visit `http://localhost:8000`
+
+
+## License
+MIT
---
+++ components/app/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+angular.module('phoneKitten', [
+  'ngFuturisticRouter',
+
+  'phoneKitten.phoneDetail',
+  'phoneKitten.phoneList',
+
+  'phoneKitten.filters',
+  'phoneKitten.services'
+]).
+controller('AppController', ['router', AppController]).
+config(['componentLoaderProvider', function (componentLoaderProvider) {
+  componentLoaderProvider.setTemplateMapping(function (name) {
+    var dashName = dashCase(name);
+    return '/components/' + dashName + '/' + dashName + '.html';
+  });
+}]).
+run(function (router, $timeout, $location) {
+  router.navigate($location.path());
+});
+
+function AppController(router) {
+  router.config([
+    { path: 'phones'          , component: 'phoneList'   },
+    { path: 'phones/:phoneId' , component: 'phoneDetail' }
+  ]);
+}
+
+function dashCase(str) {
+  return str.replace(/([A-Z])/g, function ($1) {
+    return '-' + $1.toLowerCase();
+  });
+}
